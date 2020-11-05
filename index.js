915658221bb29c9e4f6f1162071509972002a6f0
Steven McCarther <smccar5676@aol.com>
2020-11-05 18:04:57 -0500

    added functions to create readme file

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown.js");
+const fs = require("fs");
+
+const questions = [];
+
+// function to write README file
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(fileName, data);
+}
+
+// function to initialize program
+function init() {
+  inquirer.prompt(question).then((response) => {
+    console.log(response);
+    writeToFile("Readme.md", generateMarkdown(response));
+  });
+}
+
+// function call to initialize program
+init();
