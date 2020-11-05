f1c479b17cd0f5d096942e194a255903c477a57b
Steven McCarther <smccar5676@aol.com>
2020-11-05 13:54:51 -0500

    installed npm and tested with hello world

---
+++ Develop/index.js
@@ -0,0 +1,17 @@
+// array of questions for user
+const questions = [
+  
+
+];
+console.log("hello world");
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
